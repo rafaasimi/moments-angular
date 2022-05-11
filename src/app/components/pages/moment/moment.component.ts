0554a7c09de9066817d63b9b1131f7b038177d79
Rafael Simionato <rafaasimi@hotmail.com>
2022-05-11 20:25:50 -0300

    Criação da página de momento unica, estilizada e criado serviço para pegar os dados

---
+++ src/app/components/pages/moment/moment.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+import { MomentService } from 'src/app/services/moment.service';
+import { Moment } from 'src/app/Moment';
+import { Router, ActivatedRoute } from '@angular/router';
+import { environment } from 'src/environments/environment';
+
+import { faTimes, faEdit } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-moment',
+  templateUrl: './moment.component.html',
+  styleUrls: ['./moment.component.css'],
+})
+export class MomentComponent implements OnInit {
+  moment?: Moment;
+  baseApiUrl = environment.baseApiUrl;
+
+  faTimes = faTimes
+  faEdit = faEdit
+
+  constructor(
+    private momentService: MomentService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    // Recebe ID da URL
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.momentService
+      .getMoment(id)
+      .subscribe((item) => (this.moment = item.data));
+  }
+}
--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { Moment } from '../Moment';
-import {Response} from '../Response'
+import { Response } from '../Response';
 
 import { environment } from 'src/environments/environment';
 
@@ -17,12 +17,15 @@ export class MomentService {
   constructor(private http: HttpClient) {}
 
   getAllMoments(): Observable<Response<Moment[]>> {
-    return this.http.get<Response<Moment[]>>(this.apiUrl)
+    return this.http.get<Response<Moment[]>>(this.apiUrl);
+  }
+
+  getMoment(id: number): Observable<Response<Moment>> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.get<Response<Moment>>(url);
   }
 
   createMoment(formData: FormData): Observable<FormData> {
     return this.http.post<FormData>(this.apiUrl, formData);
   }
-
-  
 }
